022a3e9e0efe234e3180c3ba082dcf464949f6b3
Wills Ward <wills.e.ward@tcu.edu>
2016-04-23 20:07:05 -0500

    fixed apt error

--- auditd-master/before.sh
+++ auditd-master/before.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y audispd-plugins
+sudo apt-get update
+sudo apt-get install -y audispd-plugins
--- auditd-slave/before.sh
+++ auditd-slave/before.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y audispd-plugins
+apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y audispd-plugins
